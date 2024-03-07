12374c896a817a4f49f237f465f387162fad16df
safiqq <szarifin20041@gmail.com>
2024-03-07 23:53:09 +0700

    init project

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Halo!</Text>
+      <Text>Ini aplikasi FinoPlano</Text>
+      <Text>Karya kelompok 4 - APAAN</Text>
       <StatusBar style="auto" />
     </View>
   );
